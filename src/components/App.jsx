022f2718e381b6df51760692e167c7c4ce5a1ae1
Brian Nelson and Connor McCarthy <connor@>
2018-11-14 09:03:20 -0800

    add initial files and folders

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+
+function App(){
+  var styles = {
+  };
+  return (
+    <div style={styles}>
+      <style jsx>{`
+        font-family: Helvetica;
+      `}</style>
+      farmersMarket
+    </div>
+  );
+}
+
+//App.propTypes = {
+//};
+
+export default App;
